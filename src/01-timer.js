de1ade0e95323256ee40af3f3ec558fa59d55ffb
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-23 01:26:05 +0530

    Created a Timer with start timer functionality
    The start timer functionality will start the timer from a set time & reduce the time

--- src/01-timer.js
+++ src/01-timer.js
@@ -1,21 +1,49 @@
 let divRoot = document.getElementById("root");
 
-const p = document.createElement("p");
-p.classList.add("timer-para");
-p.innerHTML = "00:00:00";
-divRoot.appendChild(p);
+var time = document.createElement("h1");
+time.id = "time-heading";
+divRoot.appendChild(time);
 
-const startButton = document.createElement("button");
-startButton.classList.add("timer-button");
-startButton.innerHTML = "Start";
+const startButton = document.createElement("div");
+startButton.innerHTML =
+  "<input type = 'button' value = 'Start' onClick = 'time()'>";
 divRoot.appendChild(startButton);
 
-const pauseButton = document.createElement("button");
-pauseButton.classList.add("timer-button");
-pauseButton.innerHTML = "Pause";
+const pauseButton = document.createElement("div");
+pauseButton.innerHTML = "<input type = 'button' value = 'Pause' onClick = ''>";
 divRoot.appendChild(pauseButton);
 
-const resetButton = document.createElement("button");
-resetButton.classList.add("timer-button");
-resetButton.innerHTML = "Reset";
+const resetButton = document.createElement("div");
+resetButton.innerHTML = "<input type = 'button' value = 'Reset' onClick = ''>";
 divRoot.appendChild(resetButton);
+
+let hours = 0;
+let minutes = 0;
+let seconds = 2;
+
+time.innerHTML = hours + ":" + minutes + ":" + seconds;
+
+start = () => {
+  time();
+  document.getElementById("time-heading").innerHTML =
+    hours + ":" + minutes + ":" + seconds;
+  console.log(hours + ":" + minutes + ":" + seconds);
+  seconds--;
+  if (seconds < 0) {
+    seconds = 59;
+    minutes--;
+    if (minutes < 0) {
+      minutes = 59;
+      hours--;
+      if (hours < 0) {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        document.getElementById("time-heading").innerHTML =
+          "00:00:00 Timer Expired";
+      }
+    }
+  }
+};
+
+time = () => (t = setTimeout(start, 1000));
