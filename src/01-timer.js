c31426321b9e6edcf045354f3f4829b32b2b3f62
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-23 15:21:53 +0530

    Added functionality to Pause the timer

--- src/01-timer.js
+++ src/01-timer.js
@@ -6,24 +6,29 @@ let seconds = 0;
 
 let text = document.getElementById("time-heading");
 text.innerHTML = hours + ":" + minutes + ":" + seconds;
-
+let t;
 startTimer = (event) => {
-  let t = setInterval(() => {
+  t = setInterval(() => {
     text.innerHTML = hours + ":" + minutes + ":" + seconds;
-    seconds++;
-    if (seconds >= 60) {
+    if (seconds == 60) {
       seconds = 0;
       minutes++;
-      if (minutes >= 60) {
+      if (minutes == 60) {
         minutes = 0;
         hours++;
-        if (hours >= 24) {
+        if (hours == 24) {
           hours = 0;
           minutes = 0;
           seconds = 0;
           text.innerHTML = "00:00:00 Timer Expired";
+          clearInterval(t);
         }
       }
     }
+    seconds++;
   }, 1000);
+};
+
+stopTimer = (event) => {
+  clearInterval(t);
 };
