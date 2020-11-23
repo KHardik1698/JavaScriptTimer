37c9f53bd072ec39b22abeffc1378a35c53e49b0
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-23 15:02:05 +0530

    Changed the Timer functionality
    The timer will now start from a set time & increase the time. Moved the Timer Text & Buttons to HTML from JS to seperate the Logic & Elements

--- src/01-timer.js
+++ src/01-timer.js
@@ -1,49 +1,29 @@
 let divRoot = document.getElementById("root");
 
-var time = document.createElement("h1");
-time.id = "time-heading";
-divRoot.appendChild(time);
-
-const startButton = document.createElement("div");
-startButton.innerHTML =
-  "<input type = 'button' value = 'Start' onClick = 'time()'>";
-divRoot.appendChild(startButton);
-
-const pauseButton = document.createElement("div");
-pauseButton.innerHTML = "<input type = 'button' value = 'Pause' onClick = ''>";
-divRoot.appendChild(pauseButton);
-
-const resetButton = document.createElement("div");
-resetButton.innerHTML = "<input type = 'button' value = 'Reset' onClick = ''>";
-divRoot.appendChild(resetButton);
-
 let hours = 0;
 let minutes = 0;
-let seconds = 2;
+let seconds = 0;
 
-time.innerHTML = hours + ":" + minutes + ":" + seconds;
+let text = document.getElementById("time-heading");
+text.innerHTML = hours + ":" + minutes + ":" + seconds;
 
-start = () => {
-  time();
-  document.getElementById("time-heading").innerHTML =
-    hours + ":" + minutes + ":" + seconds;
-  console.log(hours + ":" + minutes + ":" + seconds);
-  seconds--;
-  if (seconds < 0) {
-    seconds = 59;
-    minutes--;
-    if (minutes < 0) {
-      minutes = 59;
-      hours--;
-      if (hours < 0) {
-        hours = 0;
+startTimer = (event) => {
+  let t = setInterval(() => {
+    text.innerHTML = hours + ":" + minutes + ":" + seconds;
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
         minutes = 0;
-        seconds = 0;
-        document.getElementById("time-heading").innerHTML =
-          "00:00:00 Timer Expired";
+        hours++;
+        if (hours >= 24) {
+          hours = 0;
+          minutes = 0;
+          seconds = 0;
+          text.innerHTML = "00:00:00 Timer Expired";
+        }
       }
     }
-  }
+  }, 1000);
 };
-
-time = () => (t = setTimeout(start, 1000));
